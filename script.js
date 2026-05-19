2c271a868dbd8478db5261121e358a00145a749b
SUDEV MAJHI <58890958+Sudev0707@users.noreply.github.com>
2026-05-20 02:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.player__video-viewer');
+const toggleBtn = document.querySelector('.player__button');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackSlider = document.querySelector('input[name="playbackRate"]');
+const rewindBtn = document.querySelector('.rewind');
+const forwardBtn = document.querySelector('.forward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    toggleBtn.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    toggleBtn.textContent = '►';
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleVolume() {
+  video.volume = this.value;
+}
+
+function handlePlayback() {
+  video.playbackRate = this.value;
+}
+
+function rewind() {
+  video.currentTime -= 10;
+}
+
+function forward() {
+  video.currentTime += 25;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+toggleBtn.addEventListener('click', togglePlay);
+progress.addEventListener('click', scrub);
+volumeSlider.addEventListener('input', handleVolume);
+playbackSlider.addEventListener('input', handlePlayback);
+rewindBtn.addEventListener('click', rewind);
+forwardBtn.addEventListener('click', forward);
+
+let mousedown = false;
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+
+video.volume = 0.75;
+video.playbackRate = 1;
